754f387779d91552da5a4c3372bca59bd830d65b
Ramukumar M <ramukumar.murugesan@in.bosch.com>
2022-10-02 09:13:04 +0200

    Home page

--- Client/src/app/components/partials/header/header.component.ts
+++ Client/src/app/components/partials/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   logo = "/assets/logo/newLogo.png"
+  @Output() isSearchSelected : boolean= false;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSearchClick():void{
+    this.isSearchSelected = true;    
   }
 
 }
